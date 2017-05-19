f2154d54997e4e9d4b7b95fe25c04e5889b7a842
Amanda Protasio <amanda.protasio@gmail.com>
2017-05-18 22:12:43 -0300

    Evolução do Config

--- scripts_config.js
+++ scripts_config.js
@@ -1,42 +1,58 @@
-<script>
-
 var inputMassa1 = document.querySelector('#massa01');
 var inputMassa2 = document.querySelector('#massa02');
 var inputMassa3 = document.querySelector('#massa03');
-var massas = document.getElementsByName('massa');
+
+var inputRecheio1 = document.querySelector('#recheio01');
+var inputRecheio2 = document.querySelector('#recheio02');
+var inputRecheio3 = document.querySelector('#recheio03');
+
+var inputCobertura1 = document.querySelector('#cobertura01');
+var inputCobertura2 = document.querySelector('#cobertura02');
+var inputCobertura3 = document.querySelector('#cobertura03');
+
+var massas    = document.getElementsByName('massa');
+var recheio   = document.getElementsByName('recheio');
+var cobertura = document.getElementsByName('cobertura');
 // var massaSelecionada = 'branco';
 
 inputMassa1.addEventListener('click', descobrirMassaSelecionada );
 inputMassa2.addEventListener('click', descobrirMassaSelecionada );
 inputMassa3.addEventListener('click', descobrirMassaSelecionada );
 
+inputRecheio1.addEventListener('click', descobrirRecheioSelecionado );
+inputRecheio2.addEventListener('click', descobrirRecheioSelecionado );
+inputRecheio3.addEventListener('click', descobrirRecheioSelecionado );
+
+inputCobertura1.addEventListener('click', descobrirCoberturaSelecionada );
+inputCobertura2.addEventListener('click', descobrirCoberturaSelecionada );
+inputCobertura3.addEventListener('click', descobrirCoberturaSelecionada );
 
 
 function descobrirMassaSelecionada(){
-
   massaSelecionada = this.id;
-//   massas.forEach(function(key) { if (key.checked == true) {
-//     massaSelecionada = key.id;
-//   };
-//
-//   }
-// )
 console.log(massaSelecionada);
 }
 
-// function calcularValorBolo(){
-//
-//   if (massaSelecionada == 'branco') {
-//     valorMassa = 25;
-//
-//   } else if (massaSelecionada == 'chocolate') {
-//     valorMassa = 30;
-//
-//   } else if (massaSelecionada == 'redvelvet') {
-//     valorMassa = 35;
-//   }
-//
-//
-// }
+function descobrirRecheioSelecionado(){
+  recheioSelecionado = this.id;
+  console.log(recheioSelecionado);
+}
 
-</script>
+function descobrirCoberturaSelecionada() {
+  coberturaSelecionada = this.Id;
+  console.log(coberturaSelecionada);
+}
+function calcularValorBolo(){
+
+  if (massaSelecionada == 'massa01') {
+    valorMassa = 25;
+
+  } else if (massaSelecionada == 'massa02') {
+    valorMassa = 30;
+
+  } else if (massaSelecionada == 'massa03') {
+    valorMassa = 35;
+  }
+
+
+}
